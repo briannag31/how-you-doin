884926c47aa943a910439795c48cde9e62efe4b5
Brianna Gaines <brianna@Briannas-MacBook-Air.local>
2022-07-21 16:17:14 -0600

    adds index page

--- main_app/models.py
+++ main_app/models.py
@@ -1,3 +1,22 @@
 from django.db import models
 
-# Create your models here.
+MOODS = (
+    ('G', 'Good Day'),
+    ('O', 'OK Day'),
+    ('B', 'Bad Day')
+)
+
+class Mood(models.Model):
+    date = models.DateField()
+    headline = models.CharField(max_length=100)
+    description = models.TextField(max_length=500)
+    designation = models.CharField(
+    max_length=1,
+    choices=MOODS,
+    default=MOODS[0][0]
+  )
+
+
+    def __str__(self):
+        return self.headline
+    
--- main_app/views.py
+++ main_app/views.py
@@ -1,7 +1,12 @@
 from django.shortcuts import render
+from .models import Mood
 
 def home(request):
   return render(request, 'home.html')
 
 def about(request):
   return render(request, 'about.html')
+
+def moods_index(request):
+    moods = Mood.objects.all()
+    return render(request, 'moods/index.html', {"moods": moods})
